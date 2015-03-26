7c8cb292ea8577c68627fc2f2a191bc93f4eb7fc
reorx <novoreorx@gmail.com>
2015-03-26 21:35:03 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: clean test
+
+clean:
+    rm -rf build *.egg-info
+
+build:
+    python setup.py build
+
+test:
+    PYTHONPATH=. nosetests -w test/ -v
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+# Use semantic versioning: MAJOR.MINOR.PATCH
+version = '0.1.0'
+
+
+def get_requires():
+    with open('requirements.txt', 'r') as f:
+        requires = [i for i in map(lambda x: x.strip(), f.readlines()) if i]
+    return requires
+
+
+def get_long_description():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    # license='License :: OSI Approved :: MIT License',
+    name='',
+    version=version,
+    author='',
+    author_email='',
+    description='',
+    long_description=get_long_description(),
+    packages=[
+    ],
+    install_requires=get_requires(),
+    # package_data={}
+    # entry_points={}
+)
